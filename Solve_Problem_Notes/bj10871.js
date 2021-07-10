34b97278d62a160113e2812e29908b4cd91c7339
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-07-11 01:19:23 +0900

    Update bj10871.js

--- Solve_Problem_Notes/bj10871.js
+++ Solve_Problem_Notes/bj10871.js
@@ -1,6 +1,30 @@
-// Node.js 에서 배열 선언하고 크기 지정해주는 방법
-// 10871 전체 코드 아님
+// 전체 코드
+
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let N = input().split(" ");
+let len = parseInt(N[0]);
+let maxNum = parseInt(N[1]);
+let arr;
+(arr = []).length = len;
+arr = input().split(" ");
+let list = arr.filter((e) => e < maxNum);
+
+console.log(list.join(" "));
+
+// 1. Node.js 에서 배열 선언하고 크기 지정해주는 방법
+
 let arr;
 (arr = []).length = 3; // 크기가 3인 숫자 배열 생성
 
 arr = input().split(" ");
+
+// 2. filter 메서드
+// arr에서 maxNum(지정한 숫자) 보다 작은 숫자들을 골라 list에 저장함
+
+let list = arr.filter((e) => e < maxNum);
